94863ab813a2215e24e0b1c22f51a9fb426e2515
Sebastian Schramm <schrammsm@gmail.com>
2023-04-13 19:14:12 +0200

    set target_modules default to None

--- finetune.py
+++ finetune.py
@@ -35,10 +35,7 @@ def train(
     lora_r: int = 8,
     lora_alpha: int = 16,
     lora_dropout: float = 0.05,
-    lora_target_modules: List[str] = [
-        "q_proj",
-        "v_proj",
-    ],
+    lora_target_modules: List[str] = None, # if None, peft will infer target modules based on model_type
     # llm hyperparams
     train_on_inputs: bool = True,  # if False, masks out inputs in loss
     group_by_length: bool = False,  # faster, but produces an odd training loss curve
